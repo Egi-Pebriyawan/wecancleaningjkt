b2d735de953aff1e0d3b8589e88b7997e1fcb39a
Egi Pebriyawan <egipebriyawan02@gmail.com>
2024-05-23 19:53:48 +0700

    Menambahkan section layanan, kenapa memilih kami dan footer serta navigasi yang tersinkronisasi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,85 +1,72 @@
-$(function() {
+$(function () {
+  // Fixed Header
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 30) {
+      $(".header").addClass("fixed-header");
+    } else $(".header").removeClass("fixed-header");
+  });
 
-    // Fixed Header 
-    $(window).scroll(function () {
-        if($(this).scrollTop() > 30)
-        {
-            $('.header').addClass('fixed-header');
-        }
-        else
-            $('.header').removeClass('fixed-header');
+  // Go Top
+  var btn = $("#top");
 
-    });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".top").removeClass("disnone");
+    } else $(".top").addClass("disnone");
+  });
 
-    // Go Top
-    var btn = $('#top');
+  btn.click(function () {
+    $("html, body").animate({ scrollTop: 0 }, "300");
+  });
 
-    $(window).scroll(function () {
-        if($(this).scrollTop() > 300)
-        {
-            $('.top').removeClass('disnone');
-        }
-        else
-            $('.top').addClass('disnone');
+  $(".show-menu").click(function () {
+    $(".header .menu-website").addClass("show");
+  });
+  $(".close-menu").click(function () {
+    $(".header .menu-website").removeClass("show");
+  });
 
-    });
+  // Trigger The Slider
+  $(".slider .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
 
-    btn.click(function(){
-        $('html, body').animate({ scrollTop:0 }, '300');
-    });
+  // Testimonial
+  $(".testimonial .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
 
-
-    $('.show-menu').click(function() {
-
-        $('.header .menu-website').addClass('show');
-
-    });
-    $('.close-menu').click(function (){
-        $('.header .menu-website').removeClass('show')
-    });
-
-    // Trigger The Slider
-    $('.slider .owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    })
-
-     // Testimonial
-     $('.testimonial .owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    })
-
-    // Slider Height
-    'use strict';
-    var winH = $(window).height(),
-        TopHeader = $('.top-header').innerHeight();
-    $('.slider .slide').height(winH - TopHeader);  
-
+  // Slider Height
+  ("use strict");
+  var winH = $(window).height(),
+    TopHeader = $(".top-header").innerHeight();
+  $(".slider .slide").height(winH - TopHeader);
 });
